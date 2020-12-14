8e8ff7e39c2409f91531c5fa27c52b4e60642473
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2020-12-14 01:22:09 +0100

    Day 11

--- 2020/day11/main.go
+++ 2020/day11/main.go
@@ -17,42 +17,91 @@ const (
 )
 
 type Place struct {
-	Type          int
-	State         int
-	NextState     int
-	AdjacentSeats []*Place
+	Type      int
+	State     int
+	NextState int
 }
 
 func main() {
 	content, _ := ioutil.ReadFile("input.txt")
 	lines := strings.Split(string(content), "\n")
 
+	parse(lines)
+
 	seats := parse(lines)
 
+	fmt.Println("occupied seats 1:", calcOccupiedSeats(lines, seats, 1, 4))
+	fmt.Println("occupied seats 2:", calcOccupiedSeats(lines, seats, -1, 5))
+}
+
+func calcOccupiedSeats(lines []string, seats map[int]map[int]*Place, maxLookAround, maxAdjacentOccupiedSeats int) int {
 	occupationChanged := true
 	for occupationChanged {
 		occupationChanged = false
-		for _, row := range seats {
-			for _, p := range row {
+		for y, line := range lines {
+			for x := range line {
+				p := seats[y][x]
 				if p.Type == SEAT {
+					lookAround := 1
+					matchInDirection := []int{-1, -1, -1, -1, -1, -1, -1, -1, -1}
+					for {
+						if maxLookAround > 0 && lookAround > maxLookAround {
+							break
+						} else if maxLookAround < 0 &&
+							y+lookAround > len(lines) &&
+							y-lookAround < 0 &&
+							x+lookAround > len(line) &&
+							x-lookAround < 0 {
+							break
+						}
+						direction := 0
+						for yi := y - lookAround; yi <= y+lookAround; yi += lookAround {
+							for xi := x - lookAround; xi <= x+lookAround; xi += lookAround {
+								if yi >= 0 &&
+									yi < len(lines) &&
+									xi >= 0 &&
+									xi < len(line) &&
+									(yi != y || xi != x) {
+									place := seats[yi][xi]
+									if place.Type == SEAT && place.State == OCCUPIED {
+										if matchInDirection[direction] < 0 {
+											matchInDirection[direction] = 1
+										}
+									} else if place.Type == SEAT && place.State == EMPTY {
+										if matchInDirection[direction] < 0 {
+											matchInDirection[direction] = 0
+										}
+									}
+								}
+								direction++
+							}
+						}
+						lookAround++
+					}
+
 					adjacentOccupiedSeats := 0
-					for _, as := range p.AdjacentSeats {
-						if as.State == OCCUPIED {
-							adjacentOccupiedSeats++
+					for _, match := range matchInDirection {
+						if match > 0 {
+							adjacentOccupiedSeats += match
 						}
 					}
+
 					if adjacentOccupiedSeats == 0 && p.State != OCCUPIED {
 						occupationChanged = true
 						p.NextState = OCCUPIED
-					} else if adjacentOccupiedSeats >= 4 && p.State != EMPTY {
+					} else if adjacentOccupiedSeats >= maxAdjacentOccupiedSeats && p.State != EMPTY {
 						occupationChanged = true
 						p.NextState = EMPTY
 					}
 				}
 			}
 		}
-		for _, row := range seats {
-			for _, s := range row {
+
+		// printSeats(lines, seats)
+
+		for y, line := range lines {
+			for x := range line {
+				s := seats[y][x]
 				s.State = s.NextState
 			}
 		}
@@ -63,49 +112,49 @@ func main() {
 		for _, s := range row {
 			if s.State == OCCUPIED {
 				occupiedSeats++
+
+				// reset seat
+				s.State = EMPTY
+				s.NextState = EMPTY
 			}
 		}
 	}
-	fmt.Println("occupied seats:", occupiedSeats)
+	return occupiedSeats
 }
 
 func parse(lines []string) map[int]map[int]*Place {
 	seats := make(map[int]map[int]*Place)
 	for y, line := range lines {
+		if seats[y] == nil {
+			seats[y] = make(map[int]*Place)
+		}
 		for x, r := range line {
-			var adjacentSeats []*Place
-			var seat *Place
 			if r == 'L' {
-				for yi := y - 1; yi < y+2; yi++ {
-					for xi := x - 1; xi < x+2; xi++ {
-						if yi >= 0 && yi < len(lines) && xi >= 0 && xi < len(line) {
-							if _, ok := seats[yi][xi]; !ok {
-								char := lines[yi][xi]
-								if char == 'L' {
-									s := &Place{Type: SEAT, NextState: EMPTY, State: EMPTY}
-									if seats[yi] == nil {
-										seats[yi] = make(map[int]*Place)
-									}
-									seats[yi][xi] = s
-								}
-							}
-							existingSeat, exists := seats[yi][xi]
-							if yi == y && xi == x {
-								seat = existingSeat
-							} else if exists {
-								adjacentSeats = append(adjacentSeats, existingSeat)
-							}
-						}
-					}
-				}
-				seat.AdjacentSeats = adjacentSeats
+				seats[y][x] = &Place{Type: SEAT, NextState: EMPTY, State: EMPTY}
 			} else {
-				if seats[y] == nil {
-					seats[y] = make(map[int]*Place)
-				}
 				seats[y][x] = &Place{Type: BOTTOM, State: EMPTY, NextState: EMPTY}
 			}
 		}
 	}
 	return seats
+}
+
+func printSeats(lines []string, seats map[int]map[int]*Place) {
+	fmt.Println()
+	fmt.Println()
+	for y, line := range lines {
+		fmt.Println()
+		for x := range line {
+			s := seats[y][x]
+			if s.Type == SEAT && s.State == OCCUPIED {
+				fmt.Print("#")
+			}
+			if s.Type == SEAT && s.State == EMPTY {
+				fmt.Print("L")
+			}
+			if s.Type == BOTTOM {
+				fmt.Print(".")
+			}
+		}
+	}
 }
